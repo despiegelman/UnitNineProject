284c954c115625190891846b8e2dbfb4c1a30b10
despiegelman <mtvesuvius67@gmail.com>
2025-03-31 07:56:45 -0400

    Finished the code

--- Tester.java
+++ Tester.java
@@ -1,8 +1,26 @@
+import java.util.ArrayList;
 public class Tester{
     public static void main (String[] args){
-        Cheese dairy = new Cheese();
+        ArrayList<Dairy> dairyShelf = new ArrayList<Dairy>();
+        Dairy dairy = new Dairy();
         Cheese dairy2 = new Cheese("cow", 31, 30, "soft", "England", "munster");
-        System.out.println(dairy.equals(dairy2));
-        System.out.println(dairy2);
+        Brie dairy3 = new Brie();
+        Dairy someCheese = new Cheese();
+        Dairy moreCheese = someCheese;
+        Dairy someBrie = new Brie("Goat", 20, 10, "round", 10);
+        Dairy moreBrie = someBrie;
+        Cheese someMoreBrie = new Brie();
+        Cheese evenMoreBrie = someMoreBrie;
+        Brie moonCheese = new Brie("cow", 31, 40, "square", 3);
+        dairyShelf.add(dairy);
+        dairyShelf.add(dairy2);
+        dairyShelf.add(dairy3);
+        dairyShelf.add(someCheese);
+        dairyShelf.add(moreCheese);
+        dairyShelf.add(someBrie);
+        dairyShelf.add(moreBrie);
+        dairyShelf.add(someMoreBrie);
+        dairyShelf.add(evenMoreBrie);
+        dairyShelf.add(moonCheese);
     }
 }
